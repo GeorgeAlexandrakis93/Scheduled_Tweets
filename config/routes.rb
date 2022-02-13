0d8b86e6fa7d5fed5a05337c0dba4a8097b49212
unknown <george-alexandrakis@hotmail.com>
2022-02-13 23:37:03 +0200

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
-  # get "", to: "main#index"
-  # get "/", to: "main#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
